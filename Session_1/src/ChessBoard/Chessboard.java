abc51993c091209ae18eace7628b3ebcd3fa0a46
SeanPlouffe <plouffesm@oldcolony.info>
2025-01-23 10:41:18 -0500

    chess

--- Session_1/src/ChessBoard/Chessboard.java
+++ Session_1/src/ChessBoard/Chessboard.java
@@ -1,4 +1,61 @@
 package ChessBoard;
 
-public class Chessboard {
+import java.util.Arrays;
+
+public class Chessboard
+{
+
+    private String[][] chessBoard = new String[8][8];
+    private final String emptySpace = "----";
+    private final String pawn = "Pawn";
+    private final String[] endRows = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"};
+
+    public void fillBoard()
+    {
+        chessBoard[0] = endRows;
+        chessBoard[chessBoard.length-1] = endRows;
+        Arrays.fill(chessBoard[1], pawn);
+        Arrays.fill(chessBoard[chessBoard.length-2], pawn);
+
+        for(int i = 2; i < chessBoard.length-2; i++)
+        {
+            Arrays.fill(chessBoard[i], emptySpace);
+        }
+    }
+
+    public void movePiece(int startRow, int startCol, int endRow, int endCol)
+    {
+
+        boolean validCoord = startRow >= 0 && startCol >= 0 && endRow >= 0 && endCol >= 0;
+
+        if(validCoord && isPiece(startRow, startCol) && isEmptySpace(endRow, endCol))
+        {
+
+            String temp = chessBoard[startRow][startCol];
+
+            chessBoard[startRow][startCol] = chessBoard[endRow][endCol];
+
+            chessBoard[endRow][endCol] = temp;
+
+        }
+        else
+        {
+            System.out.println("Invalid start location or invalid end location");
+        }
+
+    }
+
+    public boolean isPiece(int row, int col)
+    {
+        return !chessBoard[row][col].equals(emptySpace);
+    }
+
+    public boolean isEmptySpace(int row, int col)
+    {
+        return chessBoard[row][col].equals(emptySpace);
+    }
+
+    public String[][] getChessBoard() {
+        return chessBoard;
+    }
 }
--- Session_1/src/ChessBoard/Main.java
+++ Session_1/src/ChessBoard/Main.java
@@ -1,11 +1,43 @@
 package ChessBoard;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
 
+        Chessboard board = new Chessboard();
 
+        board.fillBoard();
+
+        print(board.getChessBoard());
+        System.out.println();
+
+        int pieceRow = 0, pieceCol = 0, spotRow = 0, spotCol = 0;
+
+        while(pieceRow != -1 && pieceCol != -1 && spotRow != -1 && spotCol != -1)
+        {
+
+            System.out.println("Input piece row");
+            pieceRow = input.nextInt();
+
+            System.out.println("Input piece column");
+            pieceCol = input.nextInt();
+
+            System.out.println("Input new row");
+            spotRow = input.nextInt();
+
+            System.out.println("Input new column");
+            spotCol = input.nextInt();
+
+            board.movePiece(pieceRow, pieceCol, spotRow, spotCol);
+
+            print(board.getChessBoard());
+            System.out.println();
+
+        }
 
     }
 
