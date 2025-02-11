ee41e450ddc3f6c79355896322fb26a0566f8d9e
SeanPlouffe <plouffesm@oldcolony.info>
2025-02-11 10:23:46 -0500

    Update Main.java

--- Session_1/src/ArrayPractice/Main.java
+++ Session_1/src/ArrayPractice/Main.java
@@ -13,23 +13,25 @@ public class Main
                         {3, 6, 9}
                 };
 
+        int arrLength = nums.length;
+
         // Change the final element of the 1D array at index 0 to the total length of the 2D array
 
-        updateValue(nums, 0, nums[0].length-1, nums[nums.length-1].length);
+        updateValue(nums, 0, nums[0].length-1, nums[arrLength-1].length);
 
         print(nums);
         System.out.println();
 
         // Add the value at the 1st and last index of the 2D array to the value of the last element in the 1D array at index 1
 
-        updateValue(nums, 0, nums[0].length-1, nums[0][0] + nums[nums.length-1][nums[nums.length-1].length-1]);
+        updateValue(nums, 0, nums[0].length-1, nums[0][0] + nums[arrLength-1][nums[arrLength-1].length-1]);
 
         print(nums);
         System.out.println();
 
         // Change each value of the 1D array at index 2 to display the length of each respective 1D array.
 
-        for(int i = 0; i < nums.length; i++)
+        for(int i = 0; i < arrLength; i++)
         {
             nums[i][2] = nums[i].length;
             updateValue(nums, i, 2, nums[i].length);
